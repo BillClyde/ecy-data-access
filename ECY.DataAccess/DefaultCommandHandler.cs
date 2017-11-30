a6c303dce909c7531ae7d9397d72b473eff7be7f
Clyde, Bill (ECY) <bcly461@ECY.WA.GOV>
2017-11-30 23:46:23 +0000

    Split the database class into Command and Query Handlers to make it easier to write decorators
    git-tfs-id: [http://ecytfs:8080/tfs/fass%20collection]$/FASSShared/ECY.DataAccess;C28410

---
+++ ECY.DataAccess/DefaultCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECY.DataAccess.Interfaces;
+
+namespace ECY.DataAccess
+{
+    class DefaultCommandHandler : ICommandHandler
+    {
+        private readonly ISession _session;
+
+        public DefaultCommandHandler(ISession session)
+        {
+            _session = session;
+        }
+
+        public object Execute(ICommand command)
+        {
+            return command.Execute(_session);
+        }
+    }
+}
---
+++ ECY.DataAccess/DefaultQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECY.DataAccess.Interfaces;
+
+namespace ECY.DataAccess
+{
+    public class DefaultQueryHandler : IQueryHandler
+    {
+        private ISession _session;
+
+        public DefaultQueryHandler(ISession session)
+        {
+            _session = session;
+        }
+
+        public T Query<T>(IQuery<T> query)
+        {
+            var result = query.Execute(_session);
+            return result;
+        }
+    }
+}
